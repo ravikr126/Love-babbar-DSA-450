f53d39f8a6f594823f29519ecc79fece763a48ac
Ravi Kumar <ravi14577@gmail.com>
2021-03-28 19:40:41 +0530

    Kadane's Algorithm

---
+++ Array/Kadane's Algorithm.cpp
@@ -0,0 +1,49 @@
+Kadane's Algorithm
+
+
+
+
+
+ // } Driver Code Ends
+
+
+class Solution{
+    public:
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    int maxSubarraySum(int arr[], int n){
+        
+        // Your code here
+int cs=arr[0],ms=arr[0];
+for(int i=1;i<n;i++){
+    cs=max(cs+arr[i],arr[i]);
+    ms=max(ms,cs);
+}
+       return ms;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+{
+    int t,n;
+    
+    cin>>t; //input testcases
+    while(t--) //while testcases exist
+    {
+        
+        cin>>n; //input size of array
+        
+        int a[n];
+        
+        for(int i=0;i<n;i++)
+            cin>>a[i]; //inputting elements of array
+            
+        Solution ob;
+        
+        cout << ob.maxSubarraySum(a, n) << endl;
+    }
+}
+  // } Driver Code Ends
