9caca69403e8a69d2712ee1954c54a9dfc5a067f
Ravi Kumar <ravi14577@gmail.com>
2021-06-01 14:27:45 +0530

    sorting

---
+++ Searching and sorting/First and last occurrences of x .cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+using namespace std;
+vector<int> find(int a[], int n, int x);
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, x;
+        cin >> n >> x;
+        int arr[n], i;
+        for (i = 0; i < n; i++)
+            cin >> arr[i];
+        vector<int> ans;
+        ans = find(arr, n, x);
+        cout << ans[0] << " " << ans[1] << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+vector<int> find(int arr[], int n, int x)
+{
+    // code here
+    int first = -1;
+    int last = -1;
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == x)
+        {
+            first = i;
+            last = i;
+            break;
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        if (arr[i] == x)
+            if (arr[i] == arr[i + 1])
+                last = i + 1;
+    }
+    vector<int> v;
+    v.push_back(first);
+    v.push_back(last);
+    return v;
+}
